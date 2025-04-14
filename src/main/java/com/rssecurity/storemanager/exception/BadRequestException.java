5ae30b20a2737b025ab1e374f082b304dd25f54a
anrikot <gabrieland0726@gmail.com>
2025-04-14 19:19:14 -0300

    New: Bad Request Exception

---
+++ src/main/java/com/rssecurity/storemanager/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.rssecurity.storemanager.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
