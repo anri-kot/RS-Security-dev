5c75eace8da477507d1234e72b257f3e7712c85a
anrikot <gabrieland0726@gmail.com>
2025-06-26 16:29:27 -0300

    Feat: implementing Excel Mappers

---
+++ src/main/java/com/rssecurity/storemanager/excel/mapper/CategoriaExcelMapper.java
@@ -0,0 +1,17 @@
+package com.rssecurity.storemanager.excel.mapper;
+
+import com.rssecurity.storemanager.dto.CategoriaDTO;
+
+public class CategoriaExcelMapper {
+    
+    /**
+     * Converts an Excel row into a CategoriaDTO.
+     * First row should be headers.
+     * Assumes first cell = ID (optional), second cell = Nome.
+    */
+
+    public CategoriaDTO fromRow() {
+        return null;
+    }
+
+}
